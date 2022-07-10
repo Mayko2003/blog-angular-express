ab2938d434fb5bef01ac7d8feb698364a1061352
= <=>
2022-07-10 03:14:35 -0300

    fix: changing token fetching

--- frontend/src/app/services/request-interceptor.service.ts
+++ frontend/src/app/services/request-interceptor.service.ts
@@ -1,15 +1,15 @@
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-
+import { CookieService } from 'ngx-cookie-service';
 @Injectable({
   providedIn: 'root'
 })
 export class RequestInterceptorService implements HttpInterceptor {
 
-  constructor() { }
+  constructor(private cookies: CookieService) { }
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    const token = localStorage.getItem('token');
+    const token = this.cookies.get('token');
     if (token) {
       const cloned = req.clone({
         headers: req.headers.set('Authorization', 'Bearer ' + token)
